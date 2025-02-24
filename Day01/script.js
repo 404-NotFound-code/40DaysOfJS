e50915f4a0de7e1b2890ad14479de72ce38280a3
404-NotFound-code <hello.world66134@gmail.com>
2025-02-24 22:52:46 +0600

    Day 01 completed

---
+++ Day01/script.js
@@ -0,0 +1,6 @@
+console.log("Hello, tapaScript!");
+console.log("Welcome to 40 Days of JavaScript!");
+document.write("Check the browser console for a message!");
+
+// Add content inside the html element
+document.getElementById("demo").innerText = 'This is a demo text'
---
+++ README.md
@@ -0,0 +1,9 @@
+# 40 Days of Javascript by TapaScript
+## Day 01 - Introduction to Javascript and setting up the environment
+
+### Key Learning Points
+- Setting up VS Code
+- Chrome devtools 
+- Output using console
+- Add script using async/defer 
+- document.write() 
