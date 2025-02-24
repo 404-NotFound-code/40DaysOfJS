a99d5faff85275a18e4382f4c84a402f02945cd4
404-NotFound-code <hello.world66134@gmail.com>
2025-02-24 23:15:05 +0600

    Day 02 completed

---
+++ Day02/README.md
@@ -0,0 +1,6 @@
+# 40 Days of Javascript by TapaScript
+## Day 02 - Variables and data types in JavaScript
+### Key Learning Points
+- Variables (let, const, var)
+- Primitive Data types (number, string, boolean, undefined, null, symbol, BigInt)
+- Non-primitive/reference Data types (object, function, array)
---
+++ Day02/script.js
@@ -0,0 +1,32 @@
+// Task 01: Declare variables 
+let fullName = "Proshanta Pal";
+let age = 20;
+let isStudent = true;
+let favLanguage = "JavaScript";
+
+// Task 02: Print variables in the console
+console.log(fullName);
+console.log(age);
+console.log(isStudent);
+console.log(favLanguage);
+
+// Task 03: Reassigning values using let and const
+let lang1 = "JavaScript";
+lang1 = "TypeScript";
+// let lang1 = "Python"; 
+
+const text = "I like JavaScript";
+// const text = "I like TypeScript";
+
+// Task 04: Create an object and an array
+const person = {
+    fullName: "Proshanta Pal",
+    age: 20,
+    isStudent: true 
+}
+
+console.log(person);
+console.log(person.fullName);
+
+const arr = [1,2,3,4,5];
+console.log(arr); 
